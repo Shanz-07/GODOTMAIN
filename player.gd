c2ab3752480fbd7dbf7ce24d1dac77aac958df1c
Shanz-07 <nouman8c@gmail.com>
2023-11-18 18:14:51 +0530

    v3

--- player.gd
+++ player.gd
@@ -2,14 +2,14 @@ extends CharacterBody2D
 signal rotate
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")*2.5
 var can_jump: bool = true
-var jump_speed = 1200
+var jump_speed = 1000
 func _ready():
 	pass
 func _process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 		rotate.emit()
-
+	$".".position.x+=700*delta
 	print(jump_speed)
 	if is_on_floor() and Input.is_action_pressed("up"):
 		velocity.y = -jump_speed
