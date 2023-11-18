8acfc11d29901bab177a4e64bc99d7e0d8581fcd
Shanz-07 <nouman8c@gmail.com>
2023-11-18 18:07:30 +0530

    v2

--- player.gd
+++ player.gd
@@ -1,28 +1,26 @@
 extends CharacterBody2D
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-var can_jump: bool=true
-var rotation_speed=100
+signal rotate
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")*2.5
+var can_jump: bool = true
+var jump_speed = 1200
 func _ready():
-	pass # Replace with function body.
+	pass
 func _process(delta):
-		if not is_on_floor():
-			velocity.y += (gravity) * delta
-		$".".position.x+=700*delta
-		if Input.is_action_pressed("up") and is_on_floor():
-			$".".rotation_degrees+=180 
-			$".".position.y-=200
-		move_and_slide()
+	if not is_on_floor():
+		velocity.y += gravity * delta
+		rotate.emit()
+
+	print(jump_speed)
+	if is_on_floor() and Input.is_action_pressed("up"):
+		velocity.y = -jump_speed
+	move_and_slide()
 func _on_area_body_entered(body):
 	if body is CharacterBody2D:
 		queue_free()
-	pass # Replace with function body.
 func _on_area_2d_body_entered(body):
 	if body is CharacterBody2D:
 		queue_free()
+func _on_rotate():
+	if not is_on_floor():
+		rotation_degrees+=1
 	pass # Replace with function body.
-	
-	
-	
-	
-
-	
