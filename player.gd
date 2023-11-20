7d8a2eab900790a81deb62ab9f4495f034e1415f
Shanz-07 <nouman8c@gmail.com>
2023-11-20 20:26:33 +0530

    v4

--- player.gd
+++ player.gd
@@ -1,26 +1,39 @@
 extends CharacterBody2D
-signal rotate
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")*2.5
-var can_jump: bool = true
-var jump_speed = 1000
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")*1.5
+
+var jump_speed = 600
 func _ready():
+	
 	pass
 func _process(delta):
+	
 	if not is_on_floor():
 		velocity.y += gravity * delta
-		rotate.emit()
 	$".".position.x+=700*delta
-	print(jump_speed)
-	if is_on_floor() and Input.is_action_pressed("up"):
+	
+	
+	
+	if is_on_floor() and Input.is_action_pressed("up") and $".".rotation_degrees==0:
+		var tween=create_tween()
+		tween.tween_property($".","rotation",deg_to_rad(180),0.8)
 		velocity.y = -jump_speed
+		print($".".position.y)
 	move_and_slide()
+	
+	
+	if is_on_floor() and Input.is_action_pressed("up") and $".".rotation_degrees<-90:
+		var tween=create_tween()
+		tween.tween_property($".","rotation",deg_to_rad(0),0.8)
+		velocity.y = -jump_speed*1.36
+	move_and_slide()
+	
+	
 func _on_area_body_entered(body):
 	if body is CharacterBody2D:
 		queue_free()
+		
+		
 func _on_area_2d_body_entered(body):
 	if body is CharacterBody2D:
 		queue_free()
-func _on_rotate():
-	if not is_on_floor():
-		rotation_degrees+=1
-	pass # Replace with function body.
